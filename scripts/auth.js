73c24fa6009ac420a33aeecc34d371db5fb7aa45
allicsander <me@evergoal.ru>
2020-04-27 20:40:00 +0300

    added sign in/out

--- scripts/auth.js
+++ scripts/auth.js
@@ -13,3 +13,35 @@ signUpForm.addEventListener('submit', e => {
     });
 
 });
+
+//login
+const loginForm = document.querySelector('#login-form');
+
+loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then( userCredential => {
+        console.log("happily signed in");
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+
+});
+
+
+
+//logout
+const logout = document.querySelector('#logout');
+
+logout.addEventListener('click', e => {
+    e.preventDefault();
+    auth.signOut().then( ()=> {
+       console.log("signed out");
+    });
+});
+
+
